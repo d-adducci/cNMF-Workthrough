d48537c3e35c7a2d4731c5205982074597d064e5
YourName <dominic.adducci@gmail.com>
2025-02-17 22:32:48 -0700

    Finished going through the coding vignette

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 ### After starting up environment can navigate to the appropriate working directory by first backing out of the initial directory twice:  <br/> cd .. <br/> cd .. <br/> Can then go to the appropriate working directory with the following code: <br/> cd ./mnt/c/Programming_Stuff/cNMF-Workthrough ### 
 
+Can download the data by entering the following url:<br/>
+https://cf.10xgenomics.com/samples/cell/pbmc3k/pbmc3k_filtered_gene_bc_matrices.tar.gz
 
 Packages installed in Mamba environment.
 
@@ -10,16 +12,15 @@ Version 3.10.16;
 mamba install -c conda-forge python=3.10.16
 
 Scikit-learn:
-Version 1.4.2;
-mamba install -c conda-forge scikit-learn=1.4
+Version 1.3.2;
+mamba install -c conda-forge scikit-learn=1.3
 
 scanpy:
-Version 1.8.2;
-mamba install -c bioconda scanpy=1.8
+Version 1.9.8;
+mamba install -c bioconda scanpy=1.9
 
 anndata:
-Version 0.11.3;
-mamba install conda-forge::anndata
+This was already installed with a previous package as version 11.3 which should work according to the GitHub for the cNMF package. 
 
 R:
 Version 4.4.2 "Pile of Leaves"
@@ -29,4 +30,19 @@ mamba install -c conda-forge r-base=4.4.2
 Seurat:
 Version 5.1.0;
 mamba install -c conda-forge r-seurat="5.1.0"
+
+cnmf:
+Version 1.6.0;
+mamba install bioconda::cnmf
+
+tidyverse
+Version 2.0.0;
+mamba install conda-forge::r-tidyverse
+
+openxlsx
+Version 4.2.8;
+mamba install conda-forge::r-openxlsx
+
+
+When unzipping the .gz.tar file using the system command from R I kept getting an error regarding not being able to find the file. I think this is due to using Windows subsystem for Linux. Was able to unzip, uncompress,and remove the gz.tar file with the following code <br/> gunzip filename.tar.gz <br/> tar -xf filename.tar <br/> rm filename.tar
  
--- R_vignette_tutorial.R
+++ R_vignette_tutorial.R
@@ -9,15 +9,21 @@
 library(data.table) # Version 1.16.4
 library(Matrix) # Version 1.7.2
 library(Seurat) # Version 5.1.0
+library(tidyverse) # Version 2.0.0
+library(openxlsx) # Version 4.2.8
 
-# Where zipped data file is stored
-temp_dir <- "/TempData/"
+# Where data is stored
+data_dir <- "./ProcessedData/"
 
-# Where unzipped data will be stored 
-raw_dir <- "/RawData/"
+# Where the filtered data will be stored 
+filtered_dir <- "./ProcessedData/filtered/"
+
+# Name of the run being performed
+runname <- "example_cNMF"
 
 # Where the tar.gz file is located
-file_path <- paste0(temp_dir,"pbmc_unsorted_3k_filtered_feature_bc_matrix.tar.gz")
+#file_path <- paste0(data_dir,"pbmc_unsorted_3k_filtered_feature_bc_matrix.tar.gz")
+
 
 #==============================================================================#
 # Data Setup
@@ -25,24 +31,24 @@ file_path <- paste0(temp_dir,"pbmc_unsorted_3k_filtered_feature_bc_matrix.tar.gz
 
 # I am assuming 'system' command pushes the code written in R to the 
 # environment where it can be ran in Python/non-R 
+# This code ended up not working. Pretty sure it is do to running on Windows
+# Was able to unzip and uncompress file manually from the shell. 
 
 # Extracting tar.gz file
-extract_command <- sprintf("tar -xzf %s -C %s", shQuote(file_path), 
-                           shQuote(raw_dir))
-system(extract_command) 
+#extract_command <- sprintf("tar -xzf %s -C %s", shQuote(file_path), 
+#                           shQuote(raw_dir))
+#system(extract_command) 
 
 # Removing the zip file after extracting
 # Do not need this for the way I did it
 #rm_command <- sprintf("rm %s", file_path)
 #system(rm_command)
 
-# Storing the data locally
-system("ls ./RawData/pbmc_unsorted_3k_filtered_feature_bc_matrices/hg19",
-       intern = TRUE)
+# Storing data locally
+#system("ls /filetered_gene_bc_matrices/hg19", intern = TRUE)
 
 # Data can then be loaded in with the following code
-pbmc.data <- Read10X(data.dir="/RawData/pbmc_unsorted_3k_filtered_feature_bc_matrices/hg19",
-                     intern = TRUE)
+pbmc.data <- Read10X(data.dir="./ProcessedData/filtered_gene_bc_matrices/hg19")
 
 
 
@@ -51,8 +57,7 @@ pbmc.data <- Read10X(data.dir="/RawData/pbmc_unsorted_3k_filtered_feature_bc_mat
 #==============================================================================#
 
 # Loading in raw data
-pbmc.data <- Read10X(data.dir="/RawData/pbmc_unsorted_3k_filtered_feature_bc_matrices/hg19",
-                     intern = TRUE)
+pbmc.data <- Read10X(data.dir="./ProcessedData/filtered_gene_bc_matrices/hg19")
 
 # Initializing the Seurat object with the raw (non-normalized data) 
 # Some filtering performed which removed genes detected in less than 3 cells
@@ -77,7 +82,11 @@ barcodes[1:5]
 # Saving the filtered and extracted information to the disk
 #------------------------------------------------------------------------------#
 
-filtered_dir <- "/ProcessedData/"
+filtered_dir <- "./ProcessedData/filtered/"
+
+if(!dir.exists(filtered_dir)){
+  dir.create(filtered_dir, recursive = TRUE)
+}
 
 # Output counts matrix
 writeMM(counts, paste0(filtered_dir,"matrix.mtx"))
@@ -98,7 +107,123 @@ write.table(as.data.frame(features),sep = "\t",paste0(filtered_dir,'genes.tsv'),
 # Running cNMF
 #==============================================================================#
 
+# First step is the prepare step which normalizes the count matrix and 
+# prepares the factorization step
+runname <- "example_cNMF"
+cmd <- paste("cnmf prepare --output-dir", data_dir,
+             "--name", runname,
+             "-c", paste0(filtered_dir,'matrix.mtx'),
+             "--max-nmf-iter 2000",
+             "-k 5 6 7 8 9 10 --n-iter 20", sep=" ")
+print(cmd)
+system(cmd)
 
+
+# Next is the factorization sep which runs the NMF --n-iter times (which 
+# is set to 20 in this case) for each value of K. All jobs are run 
+# sequentially here on a single worker, however these can be distributed to
+# multiple cores or nodes using separate commands such as below
+
+# cnmf factorize --output-dir ./R_Example_Data/ --name example_cNMF --worker-index 0 --total-workers 3
+# cnmf factorize --output-dir ./R_Example_Data/ --name example_cNMF --worker-index 1 --total-workers 3
+# cnmf factorize --output-dir ./R_Example_Data/ --name example_cNMF --worker-index 2 --total-workers 3
+
+# Factorization
+cmd <- paste("cnmf factorize --output-dir", data_dir,
+            "--name", runname,
+            "--worker-index 0 --total-workers 1",sep = " ")
+print(cmd)
+system(cmd)
+
+
+# Next we concatenate the results for each value of K into a single file
+cmd <- paste("cnmf combine --output-dir", data_dir,
+             "--name", runname, sep = " ")
+print(cmd)
+system(cmd)
+
+
+# A plot is made estimating the trade-off between higher value of K 
+# and stability and error
+cmd <- paste("cnmf k_selection_plot --output-dir", data_dir,
+             "--name", runname, sep = " ")
+print(cmd)
+system(cmd)
+
+
+# The plot suggest K=7 might be a local optima in stability 
+# (maximize stability, minimize error) So that will be the first option explored
+cmd <- paste("cnmf consensus --output-dir", data_dir,
+             "--name", runname,
+             "--components", 7,
+             "--local-density-threshold", 0.1,
+             "--show-clustering", sep = " ")
+print(cmd)
+system(cmd)
+
+#==============================================================================#
+# Adding to Seurat to identify GEPs
+#==============================================================================#
+
+
+# Consensus scoring looks clean, indicating that 7 is a good starting point
+usage_file <- paste(data_dir[1:length(data_dir)], runname, paste(runname, "usages", "k_7.dt_0_1","consensus","txt",sep="."),sep="/")
+spectra_score_file <- paste(data_dir[1:length(data_dir)], runname, paste(runname, "gene_spectra_score", "k_7.dt_0_1","txt",sep="."),sep="/")
+spectra_tpm_file <- paste(data_dir[1:length(data_dir)], runname, paste(runname, "gene_spectra_tpm", "k_7.dt_0_1","txt",sep="."),sep="/")
+
+usage <- read.table(usage_file, sep="\t", row.names=1, header=TRUE)
+spectra_score <- read.table(spectra_score_file, sep="\t", row.names=1, header=TRUE)
+spectra_tpm <- read.table(spectra_tpm_file, sep="\t",row.names=1, header=TRUE)
+head(usage)
+
+
+# For most analyses the resulting usage file output is normalized so 
+# that each cell sums to 1. Code below is what does that
+usage_norm <- as.data.frame(t(apply(usage, 1, function(x) x / sum(x))))
+
+
+# Concatenating the usage_norm into the metadata of the Seurat object
+# to make it easier to plot later
+new_metadata <- merge(pbmc@meta.data, usage_norm, 
+                      by = "row.names", all.x = TRUE)
+rownames(new_metadata) <- new_metadata$Row.names
+pbmc@meta.data <- new_metadata
+
+
+# Running the standard Seurat UMAP pipeline so the GEP usages can be 
+# plotted over the UMAP
+pbmc <- NormalizeData(pbmc, normalization.method = "LogNormalize", scale.factor = 10000)
+pbmc <- FindVariableFeatures(pbmc, selection.method = "vst", nfeatures = 2000)
+all.genes <- rownames(pbmc)
+pbmc <- ScaleData(pbmc, features = all.genes)
+pbmc <- RunPCA(pbmc, features = all.genes)
+pbmc <- FindNeighbors(pbmc, dims = 1:15)
+pbmc <- RunUMAP(pbmc, dims = 1:15)
+
+
+# Setting up directory to save images
+image_dir <- "./Plots/"
+
+# Plotting the learned GEPs
+p <- FeaturePlot(pbmc, features = colnames(usage_norm), combine = F)
+
+pdf(file=paste0(image_dir,"UMAP_GEPs.pdf"))
+print(p)
+dev.off()
+
+
+# Extracting the top 20 most highly weighted genes for each GEP below
+get_top_colnames <- function(row){
+  # Orders the values in descending order and gets the names of the top 20
+  print(row[1:5])
+  top_indices <- order(row, decreasing = TRUE)[1:20]
+  return(colnames(spectra_score)[top_indices])
+}
+
+top_colnames <- apply(spectra_score, 1, get_top_colnames)
+top_colnames <- as.data.frame(top_colnames)
+
+write.xlsx(x=top_colnames,file="./ProcessedData/top_20_spectra_scores.xlsx")
 
 
 
